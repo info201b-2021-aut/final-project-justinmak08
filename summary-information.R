0516989cbb9b09cfbd4f813e86176bdf554282c2
Muhd Anas <muhdanas@uw.edu>
2021-11-19 23:04:14 -0800

    changes in summ info

--- summary-information.R
+++ summary-information.R
@@ -2,9 +2,21 @@ library(dplyr)
 
 wa_electric_vehicle_population <- read.csv('Electric_Vehicle_Population_Data.csv') # Load CSV file
 
-summary_info <- list()
-summary_info$num_observations <- nrow(wa_electric_vehicle_population) # Number of vehicles registered
-summary_info$num_attributes <- ncol(wa_electric_vehicle_population) # Number of attributes per vehicle registered
-summary_info$most_expensive_vehicle <- wa_electric_vehicle_population[wa_electric_vehicle_population$Base.MSRP == max(wa_electric_vehicle_population$Base.MSRP), 8] # Most expensive electrified vehicle registered in Washington State
-summary_info$max_model_year <- max(wa_electric_vehicle_population$Model.Year) #Latest model year recorded in data set
-summary_info$min_model_year <- min(wa_electric_vehicle_population$Model.Year) #Earliest model year recorded in data set
+
+num_vehicle <- nrow(wa_electric_vehicle_population) # Number of vehicles registered
+num_attributes_vehicle <- ncol(wa_electric_vehicle_population) # Number of attributes per vehicle registered
+most_expensive <- wa_electric_vehicle_population[wa_electric_vehicle_population$Base.MSRP == max(wa_electric_vehicle_population$Base.MSRP), 8] # Most expensive electrified vehicle registered in Washington State
+latest_model <- max(wa_electric_vehicle_population$Model.Year) # Latest model year recorded in data set
+earliest_model <- min(wa_electric_vehicle_population$Model.Year) # Earliest model year recorded in data set
+
+summary_info <- list(
+  "Number of vehicles registered" = num_vehicle,
+  "Number of attributes per vehicle registered" = num_attributes_vehicle,
+  "Most expensive electrified vehicle registered in Washington State" =
+    most_expensive,
+  "Latest model year recorded in data set" =
+    latest_model,
+  "Earliest model year recorded in data set" =
+    earliest_model
+)
+
