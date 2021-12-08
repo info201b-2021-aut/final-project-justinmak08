30118070b8dcdd9309bf1a541f6889d9f78f5241
Muhd Anas <muhdanas@uw.edu>
2021-12-07 17:20:04 -0800

    Create apps

---
+++ app_server.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(shiny)
+library(plotly)
+
+# data_wa <- read.csv("")
+
+# Define server logic ----
+server <- function(input, output) {
+  
+
+
+
+
+  # Create break for Conclusion page (if necessary)
+}
---
+++ app_ui.R
@@ -0,0 +1,34 @@
+library(shiny)
+library(tidyverse)
+library(plotly)
+
+# data <- read.csv(".csv")
+
+# Create UI for Intro page
+
+
+
+
+# Create UI for interactive page 1
+
+# Create UI for interactive page 2
+
+
+
+# Create UI for interactive page 3
+
+
+
+# Create UI for conclusion page
+
+
+# Create navbar for combining all
+
+# ui <- navbarPage(
+# 
+# )
+# Define UI -----
+ui <- fluidPage(
+  
+  
+)
