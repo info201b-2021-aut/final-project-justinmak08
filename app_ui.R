05cc2bffb1c532e6869171284307dfe0239f3dea
Shannon Statler <shannonstatler@Shannons-MacBook-Air.local>
2021-12-07 19:32:43 -0800

    create all pages

--- app_ui.R
+++ app_ui.R
@@ -9,7 +9,6 @@ library(plotly)
 intro_page <- tabPanel(
   "Introduction",
   fluidPage(
-    h1("Introduction"),
     p("write introduction here"),
     img(src = "https://www.sharedmobility.news/wp-content/uploads/2017/06/CO2-emissions.jpg")
     ))
@@ -17,22 +16,38 @@ intro_page <- tabPanel(
 
 # Create UI for interactive page 1
 
+interactive_page_1 <- tabPanel(
+  "Page Title",
+  fluidPage(
+    p("interactive graph and info here")))
+
 # Create UI for interactive page 2
 
-
+interactive_page_2 <- tabPanel(
+  "Page Title",
+  fluidPage(
+    p("interactive graph and info here")))
 
 # Create UI for interactive page 3
 
+interactive_page_3 <- tabPanel(
+  "Page Title",
+  fluidPage(
+    p("interactive graph and info here")))
 
 # Create UI for conclusion page
 
+conclusion_page <- tabPanel(
+  "Summary",
+  fluidPage(
+    p("write conclusion here")))
 
 # Create navbar for combining all
 
-combine_pages <- navbarPage("CO2 Emissions", intro_page)
+combine_pages <- navbarPage("CO2 Emissions", intro_page, interactive_page_1, interactive_page_2, interactive_page_3, conclusion_page)
  
 
 # Define UI -----
-ui <- fluidPage(navbarPage("CO2 Emissions", intro_page))
+ui <- fluidPage(navbarPage("CO2 Emissions", intro_page, interactive_page_1, interactive_page_2, interactive_page_3, conclusion_page))
                 
   
