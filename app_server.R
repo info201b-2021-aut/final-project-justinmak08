6bfb32282416e4eb7f4be2bd3033ed2e9e73ccef
Justin Mak <justinmak08@gmail.com>
2021-12-08 22:34:53 -0800

    commit

--- app_server.R
+++ app_server.R
@@ -6,29 +6,38 @@ library(plotly)
 # data_wa <- read.csv("")
 wa_electric_vehicle_population <- read.csv("Electric_Vehicle_Population_Data.csv")
 
-Electric_Vehicle_Population_Data <- read.csv('Electric_Vehicle_Population_Data.csv')
-Electric_Vehicle_Population_Data_filtered <- Electric_Vehicle_Population_Data %>% filter(Model.Year != 2022)
-
 # Define server logic ----
-server <- function(input, output){
+server <- function(input, output) {
 
-#define for second interactive page
-  output$plot_2 <- renderPlot({
-    Model.Year <- (Electric_Vehicle_Population_Data_filtered[, 6])
-    bins <- seq(min(Model.Year), max(Model.Year))
-    
-    # draw the histogram
-    hist(Model.Year, breaks = bins, col = 'green', border = 'white')})
+  # Histogram
+  output$histogram_vehicle_amount <- renderPlot({
+    manufacturers <- wa_electric_vehicle_population[!duplicated(wa_electric_vehicle_population$Make),]
+    manufacturers <- select(manufacturers, Make)
+    number_vehicles <- data.frame()
+    # number_vehicles = number_vehicles[-1,]
+    for (val in manufacturers$Make) {
+      new_row <- c(val, strtoi(nrow(subset(wa_electric_vehicle_population, wa_electric_vehicle_population$Make == val))))
+      number_vehicles <- rbind(number_vehicles, new_row)
+      print(val)
+    }
+    min_number <- input$min_number
+    number_vehicles <- rename(number_vehicles, "Make" = "X.AUDI.", "Number" = "X.1678.")
+    data_histogram_number_vehicles <- filter(number_vehicles, number_vehicles$Number > min_number)
+    histogram_vehicle_amount <- ggplot(data_histogram_number_vehicles, aes(x = data_histogram_number_vehicles$Make, fill = data_histogram_number_vehicles$Make)) +
+      labs(x = "Manufacturer", y = "Number of Vehicles", title = "Number of Vehicles By Manufacturer") +
+      theme(plot.title = element_text(hjust = 0.5))
+      geom_histogram(binwidth = 1, stat = "count")
+    histogram_vehicle_amount
+  })
 
-    
+
   # Scatterplot
-  wa_electric_vehicle_population <- read.csv('Electric_Vehicle_Population_Data.csv') # Load CSV file
   full_electric_vehicle_population <- filter(wa_electric_vehicle_population, wa_electric_vehicle_population$Electric.Vehicle.Type == 'Battery Electric Vehicle (BEV)' & wa_electric_vehicle_population$Base.MSRP > 0)
 
   output$scatterplot_msrp_range <- renderPlot({
     highest_msrp <- input$max_msrp
     data_scatterplot_msrp_range <- filter(full_electric_vehicle_population, full_electric_vehicle_population$Base.MSRP <= highest_msrp)
-    scatterplot_msrp_range <- ggplot(data_scatterplot_msrp_range, aes(x=Base.MSRP, y=Electric.Range)) + # Create scatterplot
+    scatterplot_msrp_range <- ggplot(data_scatterplot_msrp_range, aes(x = Base.MSRP, y = Electric.Range)) + # Create scatterplot
       labs(x = "Vehicle MSRP", y = "Electric Range", title = "Electric Vehicle MSRP vs Range") +
       theme(plot.title = element_text(hjust = 0.5)) +
       geom_point()
--- app_ui.R
+++ app_ui.R
@@ -15,15 +15,23 @@ max_msrp <- sliderInput(
   value = 100000
 )
 
+min_number <- sliderInput(
+  inputId = "min_number",
+  h3("Only Show Manufacturers With This Many Vehicles"),
+  min = 0,
+  max = 35000,
+  value = 35000
+)
+
 # Create UI for Intro page
 intro_page <- tabPanel(
   "Introduction",
   fluidPage(
     h1("A Study on Carbon Emissions in the Pacific Northwest"),
-    p("Our domain of interest with this project is related to the environment field around the area of Pacific Northwest. We chose to observe the carbon footprint of human activities that has led towards greenhouse impacts in our local area. The questions we focused on with our research were based around specifically how vehicles are impact our local environment, and how the transition to the use of electric vehicles is moving along, and how it affects the local environment. How much of total US greenhouse gasses are released by cars with internal combustion engines? Could the change towards electric vehicles reduce the impact of carbon emission? Are more electric cars being produced? And finally, Is the range that comes with electric vehicles comparable for a feasible base MSRP so people are more open to switching to electric? To answer these questions, we analyzed data from", a(href="https://cfpub.epa.gov/ghgdata/inventoryexplorer/index.html#transportation/entiresector/allgas/category/all", "Greenhouse Gas Inventory Data Explorer"), "and from", a(href="https://data.wa.gov/Transportation/Electric-Vehicle-Population-Data/f6w7-q2d2", "Electric Vehicle Population Data"),
-    img(src = "https://www.sharedmobility.news/wp-content/uploads/2017/06/CO2-emissions.jpg")
-  )
-),)
+    p("Our domain of interest with this project is related to the environment field around the area of Pacific Northwest. We chose to observe the carbon footprint of human activities that has led towards greenhouse impacts in our local area. The questions we focused on with our research were based around specifically how vehicles are impact our local environment, and how the transition to the use of electric vehicles is moving along, and how it affects the local environment. How much of total US greenhouse gasses are released by cars with internal combustion engines? Could the change towards electric vehicles reduce the impact of carbon emission? Are more electric cars being produced? And finally, Is the range that comes with electric vehicles comparable for a feasible base MSRP so people are more open to switching to electric? To answer these questions, we analyzed data from", a(href = "https://cfpub.epa.gov/ghgdata/inventoryexplorer/index.html#transportation/entiresector/allgas/category/all", "Greenhouse Gas Inventory Data Explorer"), "and from", a(href = "https://data.wa.gov/Transportation/Electric-Vehicle-Population-Data/f6w7-q2d2", "Electric Vehicle Population Data"),
+      img(src = "https://www.sharedmobility.news/wp-content/uploads/2017/06/CO2-emissions.jpg")
+    )
+  ),)
 
 # Create UI for interactive page 1
 interactive_page_1 <- tabPanel(
@@ -32,20 +40,17 @@ interactive_page_1 <- tabPanel(
     p("interactive graph and info here")))
 
 # Create UI for interactive page 2
-year_range <- range(Electric_Vehicle_Population_Data_filtered$Model.Year)
-
 interactive_page_2 <- tabPanel(
-  "Vehicles by Model Year",
-  fluidPage(
-    h1("Number of Electric Vehicles by Model Year")),
-sidebarLayout(sidebarPanel(sliderInput("model.year", "Model Year", min = year_range[1], max = year_range[2],
-                  value = year_range,
-                  sep = "",
-                  step = 1)
-    ), mainPanel(
-      plotOutput("plot_2")
+  "Number of Vehicles by Manufacturer",
+  sidebarLayout(
+    sidebarPanel(
+      min_number
+    ),
+    mainPanel(
+      plotOutput("histogram_vehicle_amount")
     )
-  ), p("This chart helps to answer the question, “What are the trends in the number of electric and hybrid cars being produced and sold?” It displays the number of electric vehicles in Washington states, sorted by the model year of those vehicles. An upward trend can be seen through this chart, displaying that many more electric vehicles are being produced and purchased in recent years."),)
+  )
+)
 
 # Create UI for interactive page 3
 interactive_page_3 <- tabPanel(
